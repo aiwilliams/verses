f900e806a97ae37f442f5cd9321742fc5ec67b8b
Adam Williams <adam@thewilliams.ws>
2014-07-03 15:21:02 -0400

    Brand new XCode 5.1.1 project

---
+++ verses/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  verses
+//
+//  Created by Adam Williams on 7/3/14.
+//  Copyright (c) 2014 The Williams Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
