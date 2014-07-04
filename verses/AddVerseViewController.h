bdc4627431ffff4da71ad8060454b474dbd82328
Adam Williams <adam@thewilliams.ws>
2014-07-03 23:10:31 -0400

    Unwind add view

---
+++ verses/AddVerseViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddVerseViewController.h
+//  verses
+//
+//  Created by Adam Williams on 7/3/14.
+//  Copyright (c) 2014 The Williams Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddVerseViewController : UIViewController
+
+@end
---
+++ verses/VersesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  VersesTableViewController.h
+//  verses
+//
+//  Created by Adam Williams on 7/3/14.
+//  Copyright (c) 2014 The Williams Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VersesTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+@end
