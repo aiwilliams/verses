c361f1bf7b42225dc36f7d50967d81e61e2a6ece
Adam Williams <adam@thewilliams.ws>
2014-07-09 09:04:24 -0400

    XCode 6 beta 3, detail view variable height passage

--- verses/VerseDetailTableViewController.m
+++ verses/VerseDetailTableViewController.m
@@ -9,12 +9,23 @@
 #import "VerseDetailTableViewController.h"
 
 @interface VerseDetailTableViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableViewCell *passageCell;
+@property (weak, nonatomic) IBOutlet UILabel *passageTextLabel;
 @end
 
 @implementation VerseDetailTableViewController
 
 -(void)viewWillAppear:(BOOL)animated {
   self.navigationItem.title = self.biblePassage.passage;
+  self.passageTextLabel.text = @"There was a man of the Pharisees named Nicodemus, a ruler of the Jews.  2 This man came to Jesus by night and said to Him, “Rabbi, we know that You are a teacher come from God; for no one can do these signs that You do unless God is with him.” John 3:3   Jesus answered and said to him, “Most assuredly, I say to you, unless one is born again, he cannot see the kingdom of God.”";
 }
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+  CGFloat height = 1.0f; // cell separator allowance
+  if (indexPath.row == 0) {
+    height += [self.passageCell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+  }
+  return height;
+}
+
 @end
