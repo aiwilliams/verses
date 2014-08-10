b984dae267172fdfdb79e014dec19f2ce76644e0
Adam Williams <adam@thewilliams.ws>
2014-08-10 17:18:22 -0400

    Fetch and display passage content

--- verses/BiblePassage.swift
+++ verses/BiblePassage.swift
@@ -10,30 +10,30 @@ import Foundation
 import CoreData
 
 class BiblePassage : NSManagedObject {
+  @NSManaged var translation : String
   @NSManaged var passage: String
-//  @NSManaged var content : String?
+  @NSManaged var content : String
 }
 
 class BibliaAPI : NSObject {
   let managedObjectContext: NSManagedObjectContext
   let requestManager: AFHTTPRequestOperationManager
-  
+
   let apiKey = "fd37d8f28e95d3be8cb4fbc37e15e18e"
   let parseUrl = "http://api.biblia.com/v1/bible/parse"
   let contentUrl = "http://api.biblia.com/v1/bible/content/ASV.txt.json"
-  
+
   init(moc : NSManagedObjectContext) {
     managedObjectContext = moc
     requestManager = AFHTTPRequestOperationManager()
     requestManager.responseSerializer = AFJSONResponseSerializer()
   }
-  
+
   func parsePassage(passage: String, completion: (String?) -> (Void)) {
     let parameters = ["passage": passage, "key": apiKey]
     requestManager.GET(parseUrl, parameters: parameters,
       success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-        println(responseObject)
-        completion(passage)
+        completion(responseObject.valueForKey("passage") as? NSString)
       },
       failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
         completion(nil)
@@ -45,8 +45,7 @@ class BibliaAPI : NSObject {
     let parameters = ["passage": passage, "key": apiKey]
     requestManager.GET(contentUrl, parameters: parameters,
       success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-        println(responseObject)
-        completion(passage)
+        completion(responseObject.valueForKey("text") as? NSString)
       },
       failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
         completion(nil)
@@ -54,18 +53,24 @@ class BibliaAPI : NSObject {
     )
   }
 
+  // Loads the passage. If it cannot be parsed or it's content loaded, the BiblePassage will be nil.
   func loadPassage(passage: String, completion: (BiblePassage?) -> (Void) ) {
     parsePassage(passage, completion: { (normalizedPassage: String?) in
-      if normalizedPassage {
-        self.loadContentOfPassage(normalizedPassage!, completion: { (content) in
+      if let nonblankPassage = normalizedPassage {
+        self.loadContentOfPassage(nonblankPassage, completion: { (content) in
           let biblePassage = NSEntityDescription.insertNewObjectForEntityForName("BiblePassage", inManagedObjectContext: self.managedObjectContext) as BiblePassage
-          biblePassage.passage = normalizedPassage!
-//          biblePassage.content = content
-
-          var error: NSError?
-          self.managedObjectContext.save(&error)
-
-          completion(biblePassage)
+          if let nonblankContent = content {
+            biblePassage.translation = "ASV"
+            biblePassage.passage = nonblankPassage
+            biblePassage.content = nonblankContent
+            
+            var error: NSError?
+            self.managedObjectContext.save(&error)
+            
+            completion(biblePassage)
+          } else {
+            completion(nil)
+          }
         })
       } else {
         completion(nil)
--- verses/VerseDetailTableViewController.m
+++ verses/VerseDetailTableViewController.m
@@ -19,13 +19,20 @@
 
 -(void)viewWillAppear:(BOOL)animated {
   self.navigationItem.title = self.biblePassage.passage;
-  self.passageTextLabel.text = @"There was a man of the Pharisees named Nicodemus, a ruler of the Jews.  2 This man came to Jesus by night and said to Him, “Rabbi, we know that You are a teacher come from God; for no one can do these signs that You do unless God is with him.” John 3:3   Jesus answered and said to him, “Most assuredly, I say to you, unless one is born again, he cannot see the kingdom of God.”";
+  self.passageTextLabel.text = self.biblePassage.content;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+  if (section == 0)
+    return self.biblePassage.translation;
+  else
+    return @"PROGRESS";
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
   CGFloat height = 1.0f; // cell separator allowance
-  if (indexPath.row == 0) {
-    height += [self.passageCell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+  if (indexPath.section == 0 && indexPath.row == 0) {
+    height += [self.passageCell.contentView systemLayoutSizeFittingSize:UILayoutFittingExpandedSize].height;
   }
   return height;
 }
