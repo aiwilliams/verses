27aea4698d54b462813eb987062e6d33ec07ad09
ijwilliams <isaac@thewilliams.ws>
2015-03-09 16:08:41 -0400

    Further refined Reminders interface

---
+++ verses/RemindersSwitchSection.swift
@@ -0,0 +1,9 @@
+//
+//  RemindersSwitchSection.swift
+//  verses
+//
+//  Created by Isaac Williams on 3/6/15.
+//  Copyright (c) 2015 The Williams Family. All rights reserved.
+//
+
+import Foundation
