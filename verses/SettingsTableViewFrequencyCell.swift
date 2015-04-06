ede836a9f035859af8e4ac4769e41d129e6866b1
ijwilliams <isaac@thewilliams.ws>
2015-04-06 16:39:48 -0400

    Fixed missing files / Xcode Y U SO STUPID

---
+++ verses/SettingsTableViewFrequencyCell.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsTableViewFrequencyCell.swift
+//  verses
+//
+//  Created by Isaac Williams on 4/4/15.
+//  Copyright (c) 2015 The Williams Family. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewFrequencyCell: UITableViewCell {
+    @IBOutlet var picker: UIPickerView!
+}
---
+++ verses/SettingsTableViewTimeCell.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsTableViewTimeCell.swift
+//  verses
+//
+//  Created by Isaac Williams on 4/4/15.
+//  Copyright (c) 2015 The Williams Family. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewTimeCell: UITableViewCell {
+    @IBOutlet var datePicker: UIDatePicker!
+}
