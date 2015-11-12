ada86dc2c312fb7d4502d839df0b2caf96816131
ijwilliams <isaac@thewilliams.ws>
2015-11-12 17:22:23 -0500

    Get AddVerseController ready for Core Data and API requests

---
+++ Verses/AddVerseController.swift
@@ -0,0 +1,21 @@
+//
+//  AddVerseController.swift
+//  Verses
+//
+//  Created by Isaac Williams on 11/12/15.
+//  Copyright © 2015 The Williams Family. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddVerseController: UIViewController {
+    @IBOutlet var verseRequest: UITextField!
+
+    @IBAction func cancelButtonPressed(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func doneButtonPressed(sender: AnyObject) {
+    }
+}
