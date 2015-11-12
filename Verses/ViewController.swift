527b1c3b3cc6705b60197d2ec9c1d258fd4205b5
ijwilliams <isaac@thewilliams.ws>
2015-11-12 15:53:47 -0500

    Initial Commit

---
+++ Verses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Verses
+//
+//  Created by Isaac Williams on 11/12/15.
+//  Copyright © 2015 The Williams Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
