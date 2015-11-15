c266a7ea1957adf12b66a7b2a2f8e3b99b5bef6d
ijwilliams <isaac@thewilliams.ws>
2015-11-15 15:41:09 -0500

    Basic verse text fetch from HeartVersesAPI

--- Verses/AddVerseController.swift
+++ Verses/AddVerseController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 class AddVerseController: UIViewController {
+    var APIURL: String = "http://heartversesapi.herokuapp.com/api/v1"
     @IBOutlet var verseRequest: UITextField!
 
     @IBAction func cancelButtonPressed(sender: AnyObject) {
@@ -17,5 +18,28 @@ class AddVerseController: UIViewController {
     }
 
     @IBAction func doneButtonPressed(sender: AnyObject) {
+        fetchVerseTextFromAPI(verseRequest.text!)
+    }
+    
+    func fetchVerseTextFromAPI(passage: String) {
+        let verseURL = NSURL(string: self.APIURL + verseRequest.text!)!
+        let verseData: NSData? = NSData(contentsOfURL: verseURL)
+        var verseJSON: AnyObject? = nil
+
+        do {
+            verseJSON = try NSJSONSerialization.JSONObjectWithData(verseData!, options: NSJSONReadingOptions.AllowFragments)
+        } catch _ as NSError {
+            print("could not serialize JSON from HeartVersesAPI")
+        }
+        
+        if let data = verseJSON as? NSDictionary {
+            if let verses = data["verses"] as? NSArray {
+                if let topVerse = verses[0] as? NSDictionary {
+                    if let text = topVerse["text"] as? String {
+                        print(text)
+                    }
+                }
+            }
+        }
     }
 }
