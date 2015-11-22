e394aa8025570cc092668e8e92699937fb520e84
ijwilliams <isaac@thewilliams.ws>
2015-11-22 17:39:07 -0500

    Basic hint management

--- Verses/VersePracticeController.swift
+++ Verses/VersePracticeController.swift
@@ -7,24 +7,77 @@
 //
 
 import UIKit
+import CoreData
 
 class VersePracticeController: UIViewController {
-    @IBOutlet var helpLabel: UILabel!
+    @IBOutlet var basicHelpLabel: UILabel!
+    @IBOutlet var advancedHelpLabel: UILabel!
+    var passage: NSManagedObject!
+    var indexPath: NSIndexPath!
+    var hintLevel: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false
         
-        helpLabel.alpha = 0
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let moc = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Passage")
+        
+        do {
+            let results = try moc.executeFetchRequest(fetchRequest)
+            passage = results[indexPath.row] as! NSManagedObject
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        
+        title = passage.valueForKey("reference") as? String
+        basicHelpLabel.alpha = 0
+        advancedHelpLabel.alpha = 0
+        basicHelpLabel.text = passage.valueForKey("text") as? String
+        advancedHelpLabel.text = passage.valueForKey("text") as? String
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func hideWordEndings() {
+        let text = basicHelpLabel.text!
+        let attributed = NSMutableAttributedString(string: text)
+        var index = 0
+        var secondCharIndexes = [1]
+        for _ in text.characters {
+            if index < 3 {
+                ++index
+                continue
+            }
+            let backTwo = text.startIndex.advancedBy(index - 2)
+            if text.characters[backTwo] == " " {
+                secondCharIndexes.append(index)
+            }
+            ++index
+        }
+        
+        var nextIndex = 1
+        for i in secondCharIndexes {
+            if nextIndex == secondCharIndexes.count { break }
+//            print("i = \(i)")
+//            print("length = \((secondCharIndexes[nextIndex] - 2) - i))")
+            attributed.setAttributes([NSForegroundColorAttributeName:UIColor.clearColor()], range: NSMakeRange(i, (secondCharIndexes[nextIndex] - 2) - i))
+            ++nextIndex
+        }
+        basicHelpLabel.attributedText = attributed
     }
 
     @IBAction func helpButtonPressed(sender: AnyObject) {
-        UIView.animateWithDuration(1, animations: { self.helpLabel.alpha = 1 })
+        hintLevel++
+        
+        switch hintLevel {
+        case 1:
+            hideWordEndings()
+            UIView.animateWithDuration(1, animations: { self.basicHelpLabel.alpha = 1 })
+        case 2:
+            UIView.animateWithDuration(1, animations: { self.advancedHelpLabel.alpha = 1})
+        default:
+            break
+        }
     }
 }
 
--- Verses/VersesIndexController.swift
+++ Verses/VersesIndexController.swift
@@ -11,7 +11,7 @@ import UIKit
 import CoreData
 
 class VersesIndexController: UITableViewController {
-    var verses = [NSManagedObject]()
+    var passages = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +26,7 @@ class VersesIndexController: UITableViewController {
         
         do {
             let results = try moc.executeFetchRequest(fetchRequest)
-            verses = results as! [NSManagedObject]
+            passages = results as! [NSManagedObject]
         } catch let error as NSError {
             print("Could not fetch \(error), \(error.userInfo)")
         }
@@ -35,19 +35,21 @@ class VersesIndexController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return verses.count
+        return passages.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("verseCell")
-        let verse = verses[indexPath.row]
+        let verse = passages[indexPath.row]
         cell!.textLabel!.text = verse.valueForKey("reference") as? String
         return cell!
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "passagePracticeSegue" {
-            
+            let destinationViewController = segue.destinationViewController as! VersePracticeController
+            let ip: NSIndexPath = self.tableView.indexPathForCell(sender as! UITableViewCell)!
+            destinationViewController.indexPath = ip
         }
     }
 }
