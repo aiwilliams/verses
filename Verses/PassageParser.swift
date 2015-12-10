3fc64e712d48541f8781a174024c8225fa671258
ijwilliams <isaac@thewilliams.ws>
2015-12-10 17:48:59 -0500

    Handle numbered books (E.G. "2 Samuel") and return slugs

--- Verses/PassageParser.swift
+++ Verses/PassageParser.swift
@@ -17,34 +17,49 @@ struct ParsedPassage {
 }
 
 class PassageParser {
-    let books = ["genesis":"Genesis", "exodus":"Exodus", "leviticus":"Leviticus", "numbers":"Numbers", "deuteronomy":"Deuteronomy", "joshua":"Joshua", "judges":"Judges", "ruth":"Ruth", "1 samuel":"1 Samuel", "2 samuel":"2 Samuel", "1 kings":"1 Kings", "2 kings":"2 Kings", "1 chronicles":"1 Chronicles", "2 chronicles":"2 Chronicles", "ezra":"Ezra", "nehemiah":"Nehemiah", "esther":"Esther", "job":"Job", "psalms":"Psalms", "proverbs":"Proverbs", "ecclesiastes":"Ecclesiastes", "song of solomon":"Song of Solomon", "isaiah":"Isaiah", "jeremiah":"Jeremiah", "lamentations":"Lamentations", "ezekiel":"Ezekiel", "daniel":"Daniel", "hosea":"Hosea", "joel":"Joel", "amos":"Amos", "obadiah":"Obadiah", "jonah":"Jonah", "micah":"Micah", "nahum":"Nahum", "habakkuk":"Habakkuk", "zephaniah":"Zephaniah", "haggai":"Haggai", "zechariah":"Zechariah", "malachi":"Malachi",
-    
-        "matthew": "Matthew", "mark":"Mark", "luke":"Luke", "john":"John", "acts":"Acts", "romans":"Romans", "1 corinthians":"1 Corinthians", "2 corinthians":"2, Corinthians", "galatians":"Galatians", "ephesians":"Ephesians", "philippians":"Philippians", "colossians":"Colossians", "1 thessalonians":"1 Thessalonians", "2 thessalonians":"2 Thessalonians", "1 timothy":"1 Timothy", "2 timothy":"2 Timothy", "titus":"Titus", "philemon":"Philemon", "hebrews":"Hebrews", "james":"James", "1 peter":"1 Peter", "2 peter":"2 Peter", "1 john":"1 John", "2 john":"2 John", "3 john":"3 John", "jude":"Jude", "revelation":"Revelation"]
-    
     func parse(passage: String) -> ParsedPassage {
         var result = ParsedPassage()
-        let comps = passage.componentsSeparatedByCharactersInSet(NSCharacterSet(charactersInString: " :-"))
+        var comps = passage.componentsSeparatedByCharactersInSet(NSCharacterSet(charactersInString: " :-"))
+        var book: String!
         print(comps)
 
-        result.book = books[comps[0].lowercaseString]!
-        result.chapter_start = Int(comps[1])!
+        if hasNumberedBook(passage) {
+            book = convertToSlug("\(comps[0]) \(comps[1])")
+            comps.removeAtIndex(0)
+            comps.removeAtIndex(0)
+        } else {
+            book = convertToSlug(comps[0])
+            comps.removeAtIndex(0)
+        }
+
+        result.book = book
+        result.chapter_start = Int(comps[0])!
         if containsChaptersOnly(passage) {
             print("chapter only")
-            result.chapter_end = Int(comps[2])!
+            result.chapter_end = Int(comps[1])!
             result.verse_start = 1
             result.verse_end = 1
         } else if containsVerseRange(passage) {
             print("contains verse range")
-            result.chapter_end = Int(comps[1])!
-            result.verse_start = Int(comps[2])!
-            result.verse_end = Int(comps[3])!
+            result.chapter_end = Int(comps[0])!
+            result.verse_start = Int(comps[1])!
+            result.verse_end = Int(comps[2])!
         } else {
             print("single verse")
-            result.chapter_end = Int(comps[1])!
-            result.verse_start = Int(comps[2])!
-            result.verse_end = Int(comps[2])!
+            result.chapter_end = Int(comps[0])!
+            result.verse_start = Int(comps[1])!
+            result.verse_end = Int(comps[1])!
         }
         return result
+    }
+    
+    func convertToSlug(bookName: String) -> String {
+        let comps = bookName.componentsSeparatedByCharactersInSet(NSCharacterSet(charactersInString: " "))
+        if comps.count == 1 {
+            return comps[0].lowercaseString
+        } else {
+            return "\(comps[0])-\(comps[1].lowercaseString)"
+        }
     }
     
     func containsChaptersOnly(passage: String) -> Bool {
@@ -53,5 +68,23 @@ class PassageParser {
     
     func containsVerseRange(passage: String) -> Bool {
         return passage.containsString(":") && passage.containsString("-")
+    }
+    
+    func hasNumberedBook(passage: String) -> Bool {
+        let zero = passage.componentsSeparatedByCharactersInSet(NSCharacterSet(charactersInString: " :-"))[0]
+        if (Int(zero) != nil) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func containsOnlyLetters(input: String) -> Bool {
+        for chr in input.characters {
+            if (!(chr >= "a" && chr <= "z") && !(chr >= "A" && chr <= "Z") ) {
+                return false
+            }
+        }
+        return true
     }
 }
--- VersesTests/VersesTests.swift
+++ VersesTests/VersesTests.swift
@@ -25,21 +25,21 @@ class VersesTests: XCTestCase {
     
     func testParseAlreadyCleanVerse() {
         var result = parser.parse("John 1:1")
-        XCTAssertEqual("John", result.book)
+        XCTAssertEqual("john", result.book)
         XCTAssertEqual(1, result.chapter_start)
         XCTAssertEqual(1, result.chapter_end)
         XCTAssertEqual(1, result.verse_start)
         XCTAssertEqual(1, result.verse_end)
 
         result = parser.parse("John 1:2")
-        XCTAssertEqual("John", result.book)
+        XCTAssertEqual("john", result.book)
         XCTAssertEqual(1, result.chapter_start)
         XCTAssertEqual(1, result.chapter_end)
         XCTAssertEqual(2, result.verse_start)
         XCTAssertEqual(2, result.verse_end)
         
         result = parser.parse("Revelation 3:12")
-        XCTAssertEqual("Revelation", result.book)
+        XCTAssertEqual("revelation", result.book)
         XCTAssertEqual(3, result.chapter_start)
         XCTAssertEqual(3, result.chapter_end)
         XCTAssertEqual(12, result.verse_start)
@@ -48,7 +48,7 @@ class VersesTests: XCTestCase {
     
     func testParseLowercaseBookName() {
         let result = parser.parse("genesis 1:1")
-        XCTAssertEqual("Genesis", result.book)
+        XCTAssertEqual("genesis", result.book)
         XCTAssertEqual(1, result.chapter_start)
         XCTAssertEqual(1, result.chapter_end)
         XCTAssertEqual(1, result.verse_start)
@@ -57,14 +57,14 @@ class VersesTests: XCTestCase {
     
     func testParseRange() {
         var result = parser.parse("genesis 1:1-4")
-        XCTAssertEqual("Genesis", result.book)
+        XCTAssertEqual("genesis", result.book)
         XCTAssertEqual(1, result.chapter_start)
         XCTAssertEqual(1, result.chapter_end)
         XCTAssertEqual(1, result.verse_start)
         XCTAssertEqual(4, result.verse_end)
         
         result = parser.parse("john 1:3-19")
-        XCTAssertEqual("John", result.book)
+        XCTAssertEqual("john", result.book)
         XCTAssertEqual(1, result.chapter_start)
         XCTAssertEqual(1, result.chapter_end)
         XCTAssertEqual(3, result.verse_start)
@@ -73,18 +73,38 @@ class VersesTests: XCTestCase {
     
     func testParseChapterRange() {
         let result = parser.parse("psalms 1-3")
-        XCTAssertEqual("Psalms", result.book)
+        XCTAssertEqual("psalms", result.book)
         XCTAssertEqual(1, result.chapter_start)
         XCTAssertEqual(3, result.chapter_end)
         XCTAssertEqual(1, result.verse_start)
         XCTAssertEqual(1, result.verse_end)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testParseIntoProperSlug() {
+        let result = parser.parse("2 Samuel 1:3")
+        XCTAssertEqual("2-samuel", result.book)
+        XCTAssertEqual(1, result.chapter_start)
+        XCTAssertEqual(1, result.chapter_end)
+        XCTAssertEqual(3, result.verse_start)
+        XCTAssertEqual(3, result.verse_end)
+    }
+
+    func testParseNumberedBookVerseRange() {
+        let result = parser.parse("2 Samuel 1:3-4")
+        XCTAssertEqual("2-samuel", result.book)
+        XCTAssertEqual(1, result.chapter_start)
+        XCTAssertEqual(1, result.chapter_end)
+        XCTAssertEqual(3, result.verse_start)
+        XCTAssertEqual(4, result.verse_end)
+    }
+    
+    func testParseNumberedBookChapterRange() {
+        let result = parser.parse("2 Samuel 1-4")
+        XCTAssertEqual("2-samuel", result.book)
+        XCTAssertEqual(1, result.chapter_start)
+        XCTAssertEqual(4, result.chapter_end)
+        XCTAssertEqual(1, result.verse_start)
+        XCTAssertEqual(1, result.verse_end)
     }
     
 }
