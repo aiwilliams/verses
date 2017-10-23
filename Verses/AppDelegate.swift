8cc6c0c904a9e605320b83b35a4d8345dd9a716e
Isaac Williams <isaac@thewilliams.ws>
2017-10-23 15:15:07 -0400

    Sort selectedVerses fetched property by chapter and verse numbers

--- Verses/AppDelegate.swift
+++ Verses/AppDelegate.swift
@@ -1,135 +1,147 @@
-//
-//  AppDelegate.swift
-//  Verses
-//
-//  Created by Isaac Williams on 11/12/15.
-//  Copyright © 2015 The Williams Family. All rights reserved.
-//
-
 import UIKit
 import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let userDefaults = UserDefaults.standard
-        let defaultDefaults: Dictionary<String, AnyObject> = ["preferredBibleTranslation": "KJV" as AnyObject]
-        userDefaults.register(defaults: defaultDefaults)
-        
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        if !userDefaults.bool(forKey: "hasLaunchedOnce") {
-            userDefaults.set(true, forKey: "hasLaunchedOnce")
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let userDefaults = UserDefaults.standard
+    let defaultDefaults: Dictionary<String, AnyObject> = ["preferredBibleTranslation": "KJV" as AnyObject]
+    userDefaults.register(defaults: defaultDefaults)
 
-            let introductionController = mainStoryboard.instantiateViewController(withIdentifier: "IntroductionViewController") as! IntroductionViewController
-            
-            self.window?.rootViewController = introductionController
-            self.window?.makeKeyAndVisible()
-        } else {
-            let homeTabBarController = mainStoryboard.instantiateViewController(withIdentifier: "HomeTabBarController")
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
 
-            self.window?.rootViewController = homeTabBarController
-            self.window?.makeKeyAndVisible()
-        }
-        
-        let notificationSettings = UIUserNotificationSettings(types: [.alert, .sound], categories: nil)
-        UIApplication.shared.registerUserNotificationSettings(notificationSettings)
+    if !userDefaults.bool(forKey: "hasLaunchedOnce") {
+      userDefaults.set(true, forKey: "hasLaunchedOnce")
 
-        return true
+      let introductionController = mainStoryboard.instantiateViewController(withIdentifier: "IntroductionViewController") as! IntroductionViewController
+
+      self.window?.rootViewController = introductionController
+      self.window?.makeKeyAndVisible()
+    } else {
+      let homeTabBarController = mainStoryboard.instantiateViewController(withIdentifier: "HomeTabBarController")
+
+      self.window?.rootViewController = homeTabBarController
+      self.window?.makeKeyAndVisible()
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    let notificationSettings = UIUserNotificationSettings(types: [.alert, .sound], categories: nil)
+    UIApplication.shared.registerUserNotificationSettings(notificationSettings)
+
+    return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+  }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Saves changes in the application's managed object context before the application terminates.
+    self.saveContext()
+  }
+
+  // MARK: - Core Data stack
+
+  lazy var applicationDocumentsDirectory: URL = {
+    // The directory the application uses to store the Core Data store file. This code uses a directory named "ws.thewilliams.Verses" in the application's documents Application Support directory.
+    let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return urls[urls.count-1]
+  }()
+
+  lazy var managedObjectModel: NSManagedObjectModel = {
+    // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+    let modelURL = Bundle.main.url(forResource: "Verses", withExtension: "momd")!
+    let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)!
+
+    for entity in managedObjectModel.entities {
+      for property in entity.properties {
+        // Sort the selectedVerses fetched property by chapter and verse numbers
+        guard let property = property as? NSFetchedPropertyDescription,
+          let fetchRequest = property.fetchRequest,
+          fetchRequest.entityName == String(describing: UserVerse.self),
+          let entityDescription = fetchRequest.entity,
+          entityDescription.propertiesByName["chapter"] != nil,
+          entityDescription.propertiesByName["number"] != nil
+          else { continue }
+
+        let sortByChapter = NSSortDescriptor(key: "chapter", ascending: true)
+        let sortByVerse = NSSortDescriptor(key: "number", ascending: true)
+
+        fetchRequest.sortDescriptors = [sortByChapter, sortByVerse]
+      }
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    return managedObjectModel
+  }()
+
+  lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+    // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+    // Create the coordinator and store
+    let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+    let url = self.applicationDocumentsDirectory.appendingPathComponent("SingleViewCoreData.sqlite")
+    var failureReason = "There was an error creating or loading the application's saved data."
+    do {
+      let options: [AnyHashable: Any] = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
+      try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: options)
+    } catch {
+      // Report any error we got.
+      var dict = [String: AnyObject]()
+      dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject
+      dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject
+
+      dict[NSUnderlyingErrorKey] = error as NSError
+      let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+      // Replace this with code to handle the error appropriately.
+      // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+      NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+      abort()
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    return coordinator
+  }()
+
+  lazy var managedObjectContext: NSManagedObjectContext = {
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+    let coordinator = self.persistentStoreCoordinator
+    var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    managedObjectContext.persistentStoreCoordinator = coordinator
+    return managedObjectContext
+  }()
+
+  // MARK: - Core Data Saving support
+
+  func saveContext () {
+    if managedObjectContext.hasChanges {
+      do {
+        try managedObjectContext.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nserror = error as NSError
+        NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+        abort()
+      }
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var applicationDocumentsDirectory: URL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "ws.thewilliams.Verses" in the application's documents Application Support directory.
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return urls[urls.count-1]
-    }()
-
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = Bundle.main.url(forResource: "Verses", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOf: modelURL)!
-    }()
-
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.appendingPathComponent("SingleViewCoreData.sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
-            let options: [AnyHashable: Any] = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
-            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: options)
-        } catch {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject
-
-            dict[NSUnderlyingErrorKey] = error as NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-            abort()
-        }
-        
-        return coordinator
-    }()
-
-    lazy var managedObjectContext: NSManagedObjectContext = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        if managedObjectContext.hasChanges {
-            do {
-                try managedObjectContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-                abort()
-            }
-        }
-    }
+  }
 
 }
 
