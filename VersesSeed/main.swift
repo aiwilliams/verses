fc6a9434f10c154b931a54722adc754e60b2982d
Adam Williams <adam@thewilliams.ws>
2018-01-15 16:12:24 -0500

    Reformat

--- VersesSeed/main.swift
+++ VersesSeed/main.swift
@@ -10,69 +10,69 @@ let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .
 let store = HeartversesStore(sqliteURL: documentsDirectory.appendingPathComponent("Heartverses.sqlite"))
 
 func convertToSlug(bookName: String) -> String {
-    let comps = bookName.components(separatedBy: " ")
-    if comps.count == 1 {
-        return comps[0].lowercased()
-    } else if comps.count == 2 {
-        return "\(comps[0])-\(comps[1].lowercased())"
-    } else {
-        return "\(comps[0].lowercased())-\(comps[1].lowercased())-\(comps[2].lowercased())"
-    }
+  let comps = bookName.components(separatedBy: " ")
+  if comps.count == 1 {
+    return comps[0].lowercased()
+  } else if comps.count == 2 {
+    return "\(comps[0])-\(comps[1].lowercased())"
+  } else {
+    return "\(comps[0].lowercased())-\(comps[1].lowercased())-\(comps[2].lowercased())"
+  }
 }
 
 func importKJV(sourcePath: String) {
-    var json: JSON!
-    
-    do {
-        let jsonData = try Data(contentsOf: URL(fileURLWithPath: sourcePath), options: .mappedIfSafe)
-        json = try JSON(data: jsonData)
-    } catch let error {
-        print(error.localizedDescription)
+  var json: JSON!
+  
+  do {
+    let jsonData = try Data(contentsOf: URL(fileURLWithPath: sourcePath), options: .mappedIfSafe)
+    json = try JSON(data: jsonData)
+  } catch let error {
+    print(error.localizedDescription)
+  }
+  
+  let translation = "kjv"
+  var books = [Int:NSManagedObject]()
+  var chapters = [Int:Array<Int>]()
+  
+  for (_,object):(String, JSON) in json {
+    switch object["model"] {
+    case "bible.book":
+      let bookSlug = object["fields"]["slug"].stringValue
+      books[object["pk"].intValue] = store.findBook(translation, slug: bookSlug)
+    case "bible.chapter":
+      chapters[object["pk"].intValue] = [object["fields"]["book"].intValue, object["fields"]["number"].intValue]
+    case "bible.verse":
+      let chapter = chapters[object["fields"]["chapter"].intValue]!
+      let book = books[chapter[0]]!
+      store.addVerse(book, chapter: chapter[1], number: object["fields"]["number"].intValue, text: object["fields"]["text"].stringValue)
+    default:
+      print(object)
     }
-
-    let translation = "kjv"
-    var books = [Int:NSManagedObject]()
-    var chapters = [Int:Array<Int>]()
-
-    for (_,object):(String, JSON) in json {
-        switch object["model"] {
-        case "bible.book":
-            let bookSlug = object["fields"]["slug"].stringValue
-            books[object["pk"].intValue] = store.findBook(translation, slug: bookSlug)
-        case "bible.chapter":
-            chapters[object["pk"].intValue] = [object["fields"]["book"].intValue, object["fields"]["number"].intValue]
-        case "bible.verse":
-            let chapter = chapters[object["fields"]["chapter"].intValue]!
-            let book = books[chapter[0]]!
-            store.addVerse(book, chapter: chapter[1], number: object["fields"]["number"].intValue, text: object["fields"]["text"].stringValue)
-        default:
-            print(object)
-        }
-    }
+  }
 }
 
 func importNKJV(sourcePath: String) {
-    var json: JSON!
-    
-    do {
-        let jsonData = try Data(contentsOf: URL(fileURLWithPath: sourcePath), options: .mappedIfSafe)
-        json = try JSON(data: jsonData)
-    } catch let error {
-        print(error.localizedDescription)
+  var json: JSON!
+  
+  do {
+    let jsonData = try Data(contentsOf: URL(fileURLWithPath: sourcePath), options: .mappedIfSafe)
+    json = try JSON(data: jsonData)
+  } catch let error {
+    print(error.localizedDescription)
+  }
+  
+  let translation = "nkjv"
+  
+  for (_, bookObject):(String, JSON) in json {
+    let bookSlug = convertToSlug(bookName: bookObject["name"].stringValue)
+    let book = store.findBook(translation, slug: bookSlug)
+    for (_, chapterObject):(String, JSON) in bookObject["chapters"] {
+      let chapter = chapterObject["num"].intValue
+      for (_, verseObject):(String, JSON) in chapterObject["verses"] {
+        store.addVerse(book, chapter: chapter, number: verseObject["num"].intValue, text: verseObject["text"].stringValue)
+      }
     }
-
-    let translation = "nkjv"
-
-    for (_, bookObject):(String, JSON) in json {
-        let bookSlug = convertToSlug(bookName: bookObject["name"].stringValue)
-        let book = store.findBook(translation, slug: bookSlug)
-        for (_, chapterObject):(String, JSON) in bookObject["chapters"] {
-            let chapter = chapterObject["num"].intValue
-            for (_, verseObject):(String, JSON) in chapterObject["verses"] {
-                store.addVerse(book, chapter: chapter, number: verseObject["num"].intValue, text: verseObject["text"].stringValue)
-            }
-        }
-    }
+  }
 }
 
 importKJV(sourcePath: "/Users/isaacjw/Desktop/kjv_bible.json")
